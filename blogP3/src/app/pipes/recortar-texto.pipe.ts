5e36286c5e542a53893b8c4c2ae8c965b3304f6d
Pedro Bustamante Budiño <pbustamantebud@gmail.com>
2022-06-26 13:31:17 +0200

    todas las rutas entre componentes terminadas, maquetacion responsive lista, cambios menores en otros componentes

--- blogP3/src/app/pipes/recortar-texto.pipe.ts
+++ blogP3/src/app/pipes/recortar-texto.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class RecortarTextoPipe implements PipeTransform {
 
   transform(value: string): string {
-    return `${value.split(" ").slice(0, 20).join(" ")}` + `...`;
+    return `${value.split(" ").slice(0, 50).join(" ")}` + `...`;
   }
 
 }
